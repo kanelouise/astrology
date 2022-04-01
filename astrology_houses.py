5f392183f853dcb58169b01757dfc6f5a9e8cc2e
kane <kanelouise@users.noreply.github.com>
2022-03-31 17:14:15 -0700

    refactor

--- astrology_houses.py
+++ astrology_houses.py
@@ -1,35 +1,22 @@
 signs = ['Aries', 'Taurus', 'Gemini', 'Cancer', 'Leo', 'Virgo', 'Libra', 'Scorpio', 'Sagittarius', 'Capricorn', 'Aquarius', 'Pisces']
+ind = {}
+planets = ("sun", "moon", "rising")
+for planet in planets:
+	place = input(f"What is your {planet} sign? ")
+	sign_count = signs.index(place)
+	ind[f"{planet}"] = (place,sign_count)
 
-
-def find_houses():
-  global rising_sign
-  rising_sign = input("What is your rising sign? ")
-  global rising_sign_index
-  rising_sign_index = signs.index(rising_sign)
-  global houses_list
-  houses_list = signs[rising_sign_index:] + signs[:rising_sign_index]
+def find_houses(sign = ind['rising'][0], rising_ind = ind['rising'][1], signs = signs):
+  houses_list = signs[rising_ind:] + signs[:rising_ind]
   match_house = ["1st house" , '2nd house', '3rd house', '4th house', '5th house', '6th house', '7th house', '8th house', '9th house', '10th house', '11th house', '12th house']
-  match =  [str(house_num) + ": " + str(house_name) for (house_num,house_name) in zip(match_house, houses_list)]
+  match = [f"{house_num}: {house_name}" for (house_num,house_name) in zip(match_house, houses_list)]
   return(match)
 
-  
-def moon_phase():
-  global sun_sign
-  sun_sign = input("What is your sun sign? ")
-  global moon_sign
-  moon_sign = input ("What is your moon sign? ")
-  sun_sign_index = signs.index(sun_sign)
-  sun_list = signs[sun_sign_index:] + signs[:sun_sign_index]
-  sun_sign_index = sun_list.index(sun_sign)
-  moon_sign_index = sun_list.index(moon_sign)
-  global calc
-  calc = 0 - moon_sign_index
-  if calc == 6:
+def moon_phase(sun = ind['sun'], sun_ind = ind['sun'][1], moon = ind['moon'], moon_ind = ind['moon'][1]):
+  calc = moon_ind - sun_ind
+  if calc == 6 or calc == -6:
     return("You were born during a full moon.")
-  elif calc == -6:
-    return("You were born during a full moon.") 
   else:
     return("You were not born during a full moon.")
-  return(sign_calc)
 
 
